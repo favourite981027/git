2c41452f6cd8313f233f27775ca4ab65043f96a2
RuiMeng <rm@qq.com>
2021-05-26 19:42:40 +0800

    first commit

---
+++ src/main/java/com/ali/git_test/Git.java
@@ -0,0 +1,11 @@
+package com.ali.git_test;
+
+/**
+ * @author shkstart
+ * @create 2021-05-26 19:41
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("first commit");
+    }
+}
