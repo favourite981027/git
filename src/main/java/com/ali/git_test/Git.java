8420f8e21387bca96d86042d3f73deaf29ee52c6
RuiMeng <rm@qq.com>
2021-05-26 20:01:49 +0800

    hot-fix commit

--- src/main/java/com/ali/git_test/Git.java
+++ src/main/java/com/ali/git_test/Git.java
@@ -9,5 +9,6 @@ public class Git {
         System.out.println("first commit");
         System.out.println("second commit");
         System.out.println("third commit");
+        System.out.println("hot-fix");
     }
 }
